3bc42378e1c22eaf4c5fa26c10600c7832cf996c
Matteo Perini <ultra-matty@hotmail.com>
2021-06-22 16:01:23 +0200

    edit move now it flips the character based on where you are moving

--- Assets/Scripts/Interactions/Move.cs
+++ Assets/Scripts/Interactions/Move.cs
@@ -19,5 +19,16 @@ public class Move : MonoBehaviour
     {
         horizontalInput = Input.GetAxis("Horizontal");
         transform.position = new Vector2(transform.position.x + (Time.deltaTime * speed * horizontalInput), transform.position.y); //transform.Translate?
+        if (horizontalInput != 0) {
+            if (horizontalInput < 0)
+            {
+                horizontalInput = -1;
+            }
+            else
+            {
+                horizontalInput = 1;
+            }
+            transform.localScale = new Vector3(-horizontalInput, transform.localScale.y,1);
+        }
     }
 }

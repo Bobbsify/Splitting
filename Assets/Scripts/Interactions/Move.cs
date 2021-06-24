90a9af37ac9255e7bf773c47ab6d136b37398f69
Matteo Perini <ultra-matty@hotmail.com>
2021-06-24 11:00:42 +0200

    update move with canMove status

--- Assets/Scripts/Interactions/Move.cs
+++ Assets/Scripts/Interactions/Move.cs
@@ -7,6 +7,7 @@ public class Move : MonoBehaviour
     //speed of the object that's moving
     public float speed;
     private float horizontalInput;
+    public bool canMove;
 
     // Start is called before the first frame update
     void Start()
@@ -17,18 +18,20 @@ public class Move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        transform.position = new Vector2(transform.position.x + (Time.deltaTime * speed * horizontalInput), transform.position.y); //transform.Translate?
-        if (horizontalInput != 0) {
-            if (horizontalInput < 0)
-            {
-                horizontalInput = -1;
+        if (canMove) { 
+            horizontalInput = Input.GetAxis("Horizontal");
+            transform.position = new Vector2(transform.position.x + (Time.deltaTime * speed * horizontalInput), transform.position.y); //transform.Translate?
+            if (horizontalInput != 0) {
+                if (horizontalInput < 0)
+                {
+                    horizontalInput = -1;
+                }
+                else
+                {
+                    horizontalInput = 1;
+                }
+                transform.localScale = new Vector3(-horizontalInput, transform.localScale.y,1);
             }
-            else
-            {
-                horizontalInput = 1;
-            }
-            transform.localScale = new Vector3(-horizontalInput, transform.localScale.y,1);
         }
     }
 }

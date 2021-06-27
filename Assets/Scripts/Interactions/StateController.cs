a9910f07a53d9e53fa8dfe08aa15d72c2550deb6
Ventricolodx <alemleonardo@virgilio.it>
2021-06-27 18:20:29 +0200

    Add comments to StateController

--- Assets/Scripts/Interactions/StateController.cs
+++ Assets/Scripts/Interactions/StateController.cs
@@ -8,9 +8,10 @@ public class StateController : MonoBehaviour
 
     public bool isGrounded;
     public bool isWalled;
+    public bool isObstructed;
 
     public Jump jump;
-    public Move move;
+    public Move move;  // devi lasciarmi public canCrouch e crouchKey. Poi da sotto con i commenti dovresti capire come voglio gestirlo
 
     // Start is called before the first frame update
     void Start()
@@ -35,13 +36,37 @@ public class StateController : MonoBehaviour
             if (isGrounded)
             {
                 jump.canJump = true;
+                // move.canCrouch = true;
+
+                if (jump.jumpKey)
+                {
+                    move.canMove = false;
+                    //move.canCrouch = false;
+                }
+
+                /* if (move.crouchKey)
+                 * {
+                 *    jump.canJump = false;
+                 * }                
+                */
+
+                if (isObstructed) // && move.crouchKey
+                {
+                    // move.crouchKey = true;
+                }
             }
 
-            if (jump.isJumping)
-            {
+            if (!isGrounded)
+            {                
                 jump.canJump = false;
+                // move.canCrouch = false;
+
+                if (!isWalled)
+                {
+                    move.canMove = true;
+                }
             }
-        }        
+        } 
         
     }
     

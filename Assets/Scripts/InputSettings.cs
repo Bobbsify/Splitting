3fb61de2d7e2186159ae72e02d337fd126d3e8ce
Bobbsify <ultra-matty@hotmail.com>
2021-07-06 19:52:50 +0200

    add InputSettings

---
+++ Assets/Scripts/InputSettings.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputSettings
+{
+
+    [SerializeField]
+    private KeyCode jumpButton = KeyCode.Space;
+
+    [SerializeField]
+    private KeyCode pickupButton = KeyCode.LeftControl;
+
+    [SerializeField]
+    private KeyCode releaseItemButton = KeyCode.LeftControl;
+
+    [SerializeField]
+    private KeyCode undoThrowButton = KeyCode.LeftControl;
+
+    [SerializeField]
+    private KeyCode throwButton = KeyCode.LeftShift;
+
+    [SerializeField]
+    private KeyCode switchCharacterButton = KeyCode.Q;
+
+    [SerializeField]
+    private KeyCode interactButton = KeyCode.E;
+
+    [SerializeField]
+    private KeyCode hackingButton = KeyCode.F;
+
+    [SerializeField]
+    private KeyCode flashlightButton = KeyCode.T;
+
+    [SerializeField]
+    private KeyCode torchAngleUpButton = KeyCode.Alpha1;
+
+    [SerializeField]
+    private KeyCode torchAngleDownButton = KeyCode.Alpha3;
+
+}

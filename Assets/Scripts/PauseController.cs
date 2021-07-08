44cae57f48ce519090af15c1b15a8a13c4af167d
Bobbsify <ultra-matty@hotmail.com>
2021-07-08 09:39:32 +0200

    update PauseController.cs

--- Assets/Scripts/PauseController.cs
+++ Assets/Scripts/PauseController.cs
@@ -1,26 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Splitting { 
 public class PauseController : MonoBehaviour
     {
         [SerializeField]
-        public GameObject GUI;
+        private GameObject GUI;
+        [SerializeField]
+        private Button Button;
 
         private KeyCode pauseButton;
         private bool pause = false;
 
         private void Start()
         {
-            pauseButton = new InputSettings().PauseButton; 
-        }
-
-        private void OnEnable()
-        {
-            Time.timeScale = pause ? 1 : 0;
-            pause = !pause;
-            GUI.SetActive(pause);
+            pauseButton = new InputSettings().PauseButton;
+            Button.onClick.AddListener(Update);
         }
 
         private void Update()

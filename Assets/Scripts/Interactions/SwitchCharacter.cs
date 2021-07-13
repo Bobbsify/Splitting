3072e131b26107281169d13a3411c269b77dc019
Bobbsify <ultra-matty@hotmail.com>
2021-07-13 17:28:49 +0200

    update Switch character to switch off only certain scripts

--- Assets/Scripts/Interactions/SwitchCharacter.cs
+++ Assets/Scripts/Interactions/SwitchCharacter.cs
@@ -2,50 +2,62 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SwitchCharacter : MonoBehaviour
-{
-    [SerializeField] private KeyCode swapButton = KeyCode.Q; //Defaults to Q
-    [SerializeField] private GameObject targetEntity;
-
-    // Start is called before the first frame update
-    void Start()
+namespace Splitting { 
+    public class SwitchCharacter : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private GameObject targetEntity;
+        [SerializeField] private List<string> NamesOfScriptsToDisable;
+        [SerializeField] private List<string> NamesOfScriptsToEnable;
+        private KeyCode swapButton; //Defaults to Q
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (targetEntity != null)
+        // Start is called before the first frame update
+        void Awake()
         {
-            if (Input.GetKeyUp(swapButton))
+            swapButton = new InputSettings().SwitchCharacterButton;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (targetEntity != null)
             {
-                turnThisOff();
-                turnOtherOn();
+                if (Input.GetKeyUp(swapButton))
+                {
+                    turnThisOff();
+                    turnOtherOn();
+                }
             }
         }
-    }
 
-    private void turnThisOff()
-    {
-        MonoBehaviour[] scripts = gameObject.GetComponents<MonoBehaviour>();
-        foreach (MonoBehaviour script in scripts)
+        private void turnThisOff()
         {
-            script.enabled = false;
+            MonoBehaviour[] scripts = gameObject.GetComponents<MonoBehaviour>();
+            foreach (MonoBehaviour script in scripts)
+            {
+                if(NamesOfScriptsToDisable.Contains(script.name))
+                    script.enabled = false;
+            }
+            gameObject.layer = 8;
+            StopMovementX();
+            gameObject.tag = "Untagged";
         }
-        //TODO sort layer
-        gameObject.tag = "Untagged";
-    }
 
-    private void turnOtherOn()
-    {
-        MonoBehaviour[] scripts = targetEntity.GetComponents<MonoBehaviour>();
-        foreach (MonoBehaviour script in scripts)
+        private void turnOtherOn()
         {
-            script.enabled = true;
-        }
-        //TODO sort layer
-        targetEntity.tag = "Player";
+            MonoBehaviour[] scripts = targetEntity.GetComponents<MonoBehaviour>();
+            foreach (MonoBehaviour script in scripts)
+            {
+                if (NamesOfScriptsToEnable.Contains(script.name))
+                    script.enabled = true;
+            }
+            targetEntity.layer = 9;
+            targetEntity.tag = "Player";
 
+        }
+
+        private void StopMovementX()
+        {
+            GetComponent<Animator>().SetFloat("velocityX", 0f);
+        }
     }
 }

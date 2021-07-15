6382920f0e6dda291a674019523849a8b3d09aae
Ventricolodx <alemleonardo@virgilio.it>
2021-07-15 14:14:09 +0200

    create AutobotUnity script

--- Assets/Scripts/Interactions/AutobotUnity.cs
+++ Assets/Scripts/Interactions/AutobotUnity.cs
@@ -9,15 +9,20 @@ namespace Splitting
         private KeyCode connectButton;
         public bool connectable;
         private bool connectionPrep;
+        public bool readyForConnection;
         [SerializeField] private float connectDistance = 5.0f;
+        [SerializeField] private float approachSpeed;
 
         public StateController stateController;
+        private Animator animator;
 
         private Rigidbody2D tyrRig;
         private BoxCollider2D boxCol;
         private CapsuleCollider2D capsuleCol;
         private RaycastHit2D connectCheck;
         private int layerMask;
+
+        private Vector3 initialScale;
 
         // Start is called before the first frame update
         void Start()
@@ -26,6 +31,10 @@ namespace Splitting
 
             boxCol = gameObject.GetComponent<BoxCollider2D>();
             capsuleCol = gameObject.GetComponent<CapsuleCollider2D>();
+
+            animator = gameObject.GetComponent<Animator>();
+
+            initialScale = transform.localScale;
         }
 
         // Update is called once per frame
@@ -54,19 +63,43 @@ namespace Splitting
 
             if (connectionPrep)
             {
-                Debug.Log("pos1 : " + transform.position.x);
-                Debug.Log("pos2 : " + tyrRig.transform.position.x);
-
                 if (transform.position.x > tyrRig.transform.position.x)
                 {
-                    transform.position = new Vector2(transform.position.x + (Time.deltaTime * -1), transform.position.y);
-                    Debug.Log("Boh");
+                    transform.localScale = new Vector3(initialScale.x * 1, initialScale.y);
+
+                    transform.position = new Vector2(transform.position.x + (Time.deltaTime * approachSpeed *-1), transform.position.y);
+
+                    if (transform.position.x <= tyrRig.transform.position.x)
+                    {
+                        connectionPrep = false;
+                        readyForConnection = true;
+                    }
                 }
-                else if (transform.position.x <= tyrRig.transform.position.x)
+                else if (transform.position.x < tyrRig.transform.position.x)
                 {
-                    transform.position = new Vector2(transform.position.x + (Time.deltaTime * 1), transform.position.y);
-                    Debug.Log("Yes");
+                    transform.localScale = new Vector3(initialScale.x * -1, initialScale.y);
+                    transform.position = new Vector2(transform.position.x + (Time.deltaTime * approachSpeed * 1), transform.position.y);
+
+                    if (transform.position.x >= tyrRig.transform.position.x)
+                    {
+                        connectionPrep = false;
+                        readyForConnection = true;
+                    }
+                }               
+
+                if (connectionPrep)
+                {
+                    CallAnimator(Time.deltaTime * approachSpeed);
                 }
+                else
+                {
+                    CallAnimator(0);
+                }
+            }
+
+            if (readyForConnection)
+            {
+                Debug.Log("TYRANT ATTIVAZIONE!");
             }
 
 
@@ -79,6 +112,14 @@ namespace Splitting
 
             Gizmos.color = Color.red;
             Gizmos.DrawRay(new Vector2(transform.position.x - (boxCol.bounds.size.x / 2), transform.position.y + (boxCol.bounds.size.y / 2) + (capsuleCol.bounds.size.y / 8)), (Vector2.down + Vector2.right) * 5);
+        }
+
+        private void CallAnimator(float speed)
+        {
+            if (animator != null) //is null falsey?
+            {
+                animator.SetFloat("velocityX", Mathf.Abs(speed));                
+            }
         }
     }
 }

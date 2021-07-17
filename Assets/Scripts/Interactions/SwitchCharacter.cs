8719c8f55ad17c6780009faaaca7d9bacdc22a9d
Bobbsify <ultra-matty@hotmail.com>
2021-07-17 12:52:11 +0200

    fix SwitchCharacter

--- Assets/Scripts/Interactions/SwitchCharacter.cs
+++ Assets/Scripts/Interactions/SwitchCharacter.cs
@@ -16,8 +16,8 @@ namespace Splitting {
         [SerializeField] private string tyrantName;
         private GameObject tyrAnt;
         private AutobotUnity unionCheck;
+        
 
-        // Start is called before the first frame update
         void Awake()
         {
             swapButton = new InputSettings().SwitchCharacterButton;
@@ -31,33 +31,44 @@ namespace Splitting {
             }
             unionCheck = GetComponent<AutobotUnity>();
         }
+        
 
-        // Update is called once per frame
         void Update()
         {
             if (targetEntity != null)
             {
-                if (Input.GetKeyUp(swapButton) && !unionCheck.connectable)
+                if (unionCheck != null)
                 {
-                    TurnThisOff();
-                    TurnOtherOn();
+                    if (Input.GetKeyUp(swapButton) && !unionCheck.readyForConnection)
+                    {
+                        TurnThisOff();
+                        TurnOtherOn();
+                    }
+                    if (unionCheck.readyForConnection) //todo change to correct variable
+                    {
+                        Connect();
+                    }
                 }
-                if (unionCheck.connectable && false) //todo change to correct variable
+                else
                 {
-                    targetEntity.SetActive(false); //Disable Other
-                    gameObject.SetActive(false); //Disable This
-                    tyrAnt.transform.parent = null; //Remove Object from parent
-                    targetEntity.transform.parent = tyrAnt.transform; // Set Ant&Tyr as children of TyrAnt
-                    transform.parent = tyrAnt.transform;
-                    tyrAnt.SetActive(true);
-                    tyrAnt.GetComponent<Animator>().SetTrigger("Link");
+                    if (Input.GetKeyUp(swapButton))
+                    {
+                        TurnThisOff();
+                        TurnOtherOn();
+                    }
                 }
             }
         }
 
         private void Connect()
         {
-            
+            targetEntity.SetActive(false); //Disable Other
+            gameObject.SetActive(false); //Disable This
+            tyrAnt.transform.parent = null; //Remove Object from parent
+            targetEntity.transform.parent = tyrAnt.transform; // Set Ant&Tyr as children of TyrAnt
+            transform.parent = tyrAnt.transform;
+            tyrAnt.SetActive(true);
+            tyrAnt.GetComponent<Animator>().SetTrigger("Link");
         }
 
         private void TurnThisOff()

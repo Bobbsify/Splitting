111fb24df7eaefe9b7532600e032525fd43caf14
Bobbsify <ultra-matty@hotmail.com>
2021-07-19 11:55:14 +0200

    fix killArea

--- Assets/Scripts/Interactions/KillArea.cs
+++ Assets/Scripts/Interactions/KillArea.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class KillArea : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         Animator anim = collision.transform.GetComponent<Animator>();
         try

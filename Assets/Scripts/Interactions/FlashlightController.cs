c175a4ba70663e78e5bb22eebb80fe031845c941
Bobbsify <ultra-matty@hotmail.com>
2021-08-18 11:04:23 +0200

    fix tyr prefab & flashlight

--- Assets/Scripts/Interactions/FlashlightController.cs
+++ Assets/Scripts/Interactions/FlashlightController.cs
@@ -1,27 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
 
 namespace Splitting { 
     public class FlashlightController : MonoBehaviour
     {
         public float rotationAmount = 1.0f;
+        
+        [HideInInspector] public bool canMoveFlashlight;
 
         private float angle;
+        private List<Light2D> flashlights = new List<Light2D>();
 
         private KeyCode rotateClockwise;
         private KeyCode rotateCounterClockwise;
+        private KeyCode toggleFlashlight;
 
         private void Start()
         {
             rotateClockwise = new InputSettings().TorchAngleUpButton;
             rotateCounterClockwise = new InputSettings().TorchAngleDownButton;
+            toggleFlashlight = new InputSettings().FlashlightButton;
+
+            foreach (Transform child in transform)
+            {
+                Light2D light = child.GetComponent<Light2D>();
+                if (light != null)
+                {
+                    flashlights.Add(light);
+                }
+            }
 
             angle = transform.localRotation.z;
         }
 
         private void Update()
         {
+            if (Input.GetKeyUp(toggleFlashlight))
+            {
+                foreach (Light2D light in flashlights)
+                {
+                    light.enabled = !light.enabled;
+                }
+            }
             if (Input.GetKey(rotateClockwise))
             {
                 angle += rotationAmount * Time.deltaTime;

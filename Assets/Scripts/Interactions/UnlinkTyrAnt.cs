c0e81d5d78b47a15eeb0bb8d84414d9d29978c6a
Bobbsify <ultra-matty@hotmail.com>
2021-08-20 10:37:34 +0200

    Revert "fix direction on instantiation"
    This reverts commit 617d262e8d125d152c28fd1081c11af30d31ad0b.

--- Assets/Scripts/Interactions/UnlinkTyrAnt.cs
+++ Assets/Scripts/Interactions/UnlinkTyrAnt.cs
@@ -62,14 +62,12 @@ namespace Splitting
             ant.GetComponent<AutobotUnity>().enabled = true;
             ant.GetComponent<AutobotUnity>().GetStateControllers();
             ant.GetComponent<SwitchCharacter>().enabled = true;
-            ant.transform.localScale = ant.transform.localScale.z * transform.localScale;
         }
 
         private void SetupTyr(GameObject tyr)
         {
             tyr.GetComponent<AutobotUnity>().enabled = true;
             tyr.GetComponent<AutobotUnity>().GetStateControllers();
-            tyr.transform.localScale = new Vector3(tyr.transform.localScale.x * transform.localScale.x, tyr.transform.localScale.y, 1);
         }
     }
 }

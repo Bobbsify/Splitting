b8d7d8505e2398bf6009cb5c33dbc52e48a0fedd
Ventricolodx <alemleonardo@virgilio.it>
2021-09-14 10:27:18 +0200

    fix Fear and Hacking interactions

--- Assets/Scripts/Interactions/Fear.cs
+++ Assets/Scripts/Interactions/Fear.cs
@@ -12,6 +12,9 @@ namespace Splitting
         public FlashlightController flashlightController;
         public Collider2D lightAreaCol;
 
+        [SerializeField] private float elapsedInDark;
+        [SerializeField] private float timerInDark = 1.5f;
+
         // Start is called before the first frame update
         void Start()
         {
@@ -27,6 +30,8 @@ namespace Splitting
 
         private void OnTriggerStay2D(Collider2D collision)
         {
+            elapsedInDark = 0.0f;
+
             bool ant = false;
             bool tyr = false;          
 
@@ -49,20 +54,28 @@ namespace Splitting
 
         private void OnTriggerExit2D(Collider2D collision)
         {
+            elapsedInDark += Time.deltaTime;
+
             bool ant = false;
             bool tyr = false;
 
             ant = collision.name.ToUpper().Contains("ANT");
             tyr = collision.name.ToUpper().Contains("TYR");
 
-            if (ant && !tyr)
-            {               
-                antStateController.isIlluminated = false;
+            if (elapsedInDark >= timerInDark)
+            {
+                if (ant && !tyr)
+                {
+                    antStateController.isIlluminated = false;
+                }
+                else if (ant && tyr)
+                {
+                    tyrantStateController.isIlluminated = false;
+                }
+
+                elapsedInDark = 0.0f;
             }
-            else if (ant && tyr)
-            {              
-                tyrantStateController.isIlluminated = false;
-            }
+           
         }
         
         void ControlLightStatus()

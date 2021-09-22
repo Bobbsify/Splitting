dbd79190f7803b64c22e27533e80ac84aa5fbb0d
Bobbsify <ultra-matty@hotmail.com>
2021-09-22 16:30:05 +0200

    add interaction detection

--- Assets/Scripts/Behaviours/InteractionCheck.cs
+++ Assets/Scripts/Behaviours/InteractionCheck.cs
@@ -4,36 +4,43 @@ using UnityEngine;
 
 public class InteractionCheck : MonoBehaviour
 {
-    [SerializeField] private List<string> tagsToCheck = new List<string> { "Carryable", "Interactable" };
-    [Tooltip("list of prefabs to show for each tag (element 0 corresponds to element 0 of other list)")]
-    [SerializeField] private List<GameObject> buttonToShow;
+    [SerializeField] private GameObject interactionPrefab;
+    private GameObject interactionPrefabInstantiated;
+    private bool isPlayerInRange = false;
 
-    [SerializeField] private Collider2D wallCheck;
-    private List<GameObject> interactableObjects;
-    private List<GameObject> buttonsShown;
+    public void Update()
+    {
+        if (isPlayerInRange)
+        {
+            if (interactionPrefabInstantiated == null)
+            {
+                interactionPrefabInstantiated = Instantiate(interactionPrefab);
+                interactionPrefabInstantiated.transform.parent = transform;
+                Collider2D col;
+                TryGetComponent(out col);
+                interactionPrefabInstantiated.transform.localPosition = new Vector3(0, col.bounds.extents.y, 0);
+            }
+        }
+        else
+        {
+            Destroy(interactionPrefabInstantiated);
+            interactionPrefabInstantiated = null;
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        foreach (string tag in tagsToCheck)
+        if (collision.gameObject.tag.ToLower() == "player")
         {
-            if (collision.tag.ToLower() == tag.ToLower())
-            {
-                interactableObjects.Add(collision.gameObject);
-                GameObject showBtn = Instantiate(buttonsShown.ToArray()[tagsToCheck.IndexOf(tag)]);
-                buttonsShown.Add(showBtn);
-            }
+            isPlayerInRange = true;
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        foreach (string tag in tagsToCheck)
+        if (collision.gameObject.tag.ToLower() == "player")
         {
-            if (collision.tag.ToLower() == tag.ToLower())
-            {
-                buttonsShown.RemoveRange(interactableObjects.IndexOf(collision.gameObject),1);
-                interactableObjects.Remove(collision.gameObject);
-            }
+            isPlayerInRange = false;
         }
     }
 }

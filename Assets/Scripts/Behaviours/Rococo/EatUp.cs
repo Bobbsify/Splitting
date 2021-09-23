a45ce67c031b47857bdf49b20f3c309d55848834
Bobbsify <ultra-matty@hotmail.com>
2021-09-23 15:45:00 +0200

    fix Eatup

--- Assets/Scripts/Behaviours/Rococo/EatUp.cs
+++ Assets/Scripts/Behaviours/Rococo/EatUp.cs
@@ -35,16 +35,16 @@ namespace Splitting {
                 Vector3 playerPos = collision.transform.position;
                 if (playerPos.y >= rococoTransform.position.y - skipToBiteOffset && playerPos.y <= rococoTransform.position.y + skipToBiteOffset)
                 {
-                    Debug.Log("original rococo " + originalRococo);
-                    Debug.Log("rococo's dad " + originalRococo.Find(rococoIdleAttackName));
-                    originalRococo.Find(rococoIdleAttackName).transform.parent = null; //remove from this obj
-                    originalRococo.Find(rococoIdleAttackName).gameObject.SetActive(true); //activate obj
+                    Transform activatedRococo = originalRococo.Find(rococoIdleAttackName);
+                    activatedRococo.transform.parent = null; //remove from this obj
+                    activatedRococo.gameObject.SetActive(true); //activate obj
                     Destroy(originalRococo.gameObject);
                 }
                 else
                 {
-                    originalRococo.Find(rococoJumpAttackName).transform.parent = null; //remove from this obj
-                    originalRococo.Find(rococoJumpAttackName).gameObject.SetActive(true); // activate obj
+                    Transform activatedRococo = originalRococo.Find(rococoJumpAttackName);
+                    activatedRococo.transform.parent = null; //remove from this obj
+                    activatedRococo.gameObject.SetActive(true); //activate obj
                     Destroy(originalRococo.gameObject);
                 }
             }

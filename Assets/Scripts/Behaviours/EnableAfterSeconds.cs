8be7bb95b8e13fcda6f602826b089b4c4dcfaa51
Bobbsify <ultra-matty@hotmail.com>
2021-10-02 22:54:10 +0200

    add EnableAfterSeconds script

---
+++ Assets/Scripts/Behaviours/EnableAfterSeconds.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableAfterSeconds : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> objsToEnable;
+    [SerializeField] private float delayInSeconds = 2.0f;
+
+    private void Awake()
+    {
+        StartCoroutine(doEnabling());
+    }
+
+    private IEnumerator doEnabling()
+    {
+        yield return new WaitForSecondsRealtime(delayInSeconds);
+        foreach (GameObject obj in objsToEnable)
+        {
+            obj.SetActive(true);
+        }
+    }
+}
